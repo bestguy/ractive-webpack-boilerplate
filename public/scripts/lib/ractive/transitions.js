f366b9dd101fb2f398f6488421546aa09b1822ca
Gary Thomas <garlos@gmail.com>
2015-05-06 08:23:24 -0700

    Fix transitions

---
+++ public/scripts/lib/ractive/transitions.js
@@ -0,0 +1,4 @@
+var Ractive = require('ractive');
+Ractive.transitions.fade = require('ractive-transitions-fade');
+Ractive.transitions.slide = require('ractive-transitions-slide');
+require('./transitions/animate.js');

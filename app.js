89fb55b226872f70c828b7f5f5567b591bc805b0
Gary Thomas <gary@gary.local>
2015-10-17 11:10:47 -0700

    Use ES2015 support in node 4.x

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var config = require('config');
-var express = require('express');
-var logger = require('morgan');
+'use strict';
 
-var app = new express();
+let config = require('config');
+let express = require('express');
+let logger = require('morgan');
+
+let app = new express();
 
 // ## Middleware
 
 app.use(logger('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
 // ## Routes
 if (config.env == 'dev') {
-  var webpackMiddleware = require('webpack-dev-middleware');
-  var webpack = require('webpack');
-  var webpackConfig = require('./webpack.config.js');
+  let webpackMiddleware = require('webpack-dev-middleware');
+  let webpack = require('webpack');
+  let webpackConfig = require('./webpack.config.js');
   app.use(webpackMiddleware(webpack(webpackConfig), {}));
 }
 
-app.listen(config.port, function () {
-  console.log('Server running...');
-});
+app.listen(config.port, () => console.log(`Server running on port ${config.port}...`));

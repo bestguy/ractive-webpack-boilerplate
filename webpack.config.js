20a78022d080300d029082b46700e1e1213b1f19
Gary Thomas <gary@gary.local>
2015-10-23 09:57:12 -0700

    ES2015 for webpack config, use Ractive loader for jade files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-var webpack = require('webpack');
-var BASE = __dirname + '/public/scripts/';
+'use strict';
+let webpack = require('webpack');
+const BASE = `${__dirname}/public/scripts`;
 
 module.exports = {
-  entry: BASE + 'main.es6',
+  entry: `${BASE}/main.es6`,
   output: {
-    path: __dirname + '/public/',
+    path: `${__dirname}/public/`,
     filename: 'bundle.js'
   },
   module: {
@@ -12,28 +13,24 @@ module.exports = {
       { test: /\.css/, loader: 'style-loader!css-loader!autoprefixer-loader' },
       { test: /\.es6/, loader: 'babel-loader' },
       { test: /\.less/, loader: 'style-loader!css-loader!autoprefixer-loader!less-loader' },
-      { test: /\.jade$/, loader: 'html!jade-html' },
+      { test: /\.jade$/, loader: 'ractive!jade-html' },
       { test: /\.json/, loader: 'json-loader' },
       { test: /\.html/, loader: 'ractive' }
     ]
   },
   resolve: {
-    root: BASE + 'bower_components',
+    root: `${BASE}/bower_components`,
     alias: {
                         pubsub: 'aurelia-event-aggregator',
                        Ractive: 'ractive',
-    'ractive-transitions-fade': BASE + 'bower_components/ractive-transitions-fade/dist/ractive-transitions-fade.js',
-                  'underscore': 'lodash',
-                'webpack-i18n': BASE + 'lib/webpack/webpack-i18n.js'
+    'ractive-transitions-fade': `${BASE}/bower_components/ractive-transitions-fade/dist/ractive-transitions-fade.js`,
+                  'underscore': 'lodash', // old habits die hard
+                'webpack-i18n': `${BASE}/lib/webpack/webpack-i18n.js`
     }
   },
   plugins: [
     new webpack.ResolverPlugin(
       new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin('bower.json', ['main'])
-    ),
-    new webpack.ProvidePlugin({
-     $: 'jquery',
-     jQuery: 'jquery'
-   })
+    )
   ]
 };

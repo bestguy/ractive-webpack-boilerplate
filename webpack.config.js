e7b7737a7873ee2b7683d547f113dba426e71b94
Gary Thomas <garlos@gmail.com>
2016-07-16 21:21:17 -0700

    Update dependencies, web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,45 @@
-const BASE = `${__dirname}/public/scripts`;
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: `${BASE}/main.es6`,
+  entry: [
+    path.resolve(__dirname, './public/scripts/main.es6')
+  ],
   output: {
-    path: `${__dirname}/public/`,
+    path: path.resolve(__dirname, './public'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.css/, loader: 'style-loader!css-loader!autoprefixer-loader' },
+      { test: /\.css/, loader: 'style!css!postcss' },
       {
         test: /\.(es6|js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loaders: ['babel'],
       },
-      { test: /\.less/, loader: 'style-loader!css-loader!autoprefixer-loader!less-loader' },
+      { test: /\.less/, loader: 'style!css!postcss!less-loader' },
       { test: /\.jade$/, loader: 'ractive!jade-html' },
       { test: /\.json/, loader: 'json-loader' },
       { test: /\.html/, loader: 'ractive' }
     ]
   },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  postcss: [
+    autoprefixer({ browsers: ['last 2 versions'] })
+  ],
   resolve: {
     alias: {
       pubsub: 'aurelia-event-aggregator',

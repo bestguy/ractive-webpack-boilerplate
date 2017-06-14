51f3957e122732400f108fd1152e617a9335aa36
Gary Thomas <garlos@gmail.com>
2017-06-14 10:53:48 -0700

    Update dependencies
    - Update Webpack config
    - Update PostCSS config
    - Add HTML plugin to eslint

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -11,7 +11,7 @@ config.entry.unshift('webpack-hot-middleware/client');
 config.plugins = [
   new webpack.optimize.OccurrenceOrderPlugin(),
   new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin(),
+  new webpack.NoEmitOnErrorsPlugin(),
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify('development')
